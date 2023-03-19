077b8bdaae36c82129d473c3f2ba47fdda202a25
drodsou <gitman@drodsou.com>
2023-03-19 06:37:32 +0100

    first

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ import {compReplace, $} from './comp/comp.js';
 import Counter from './comp/Counter.js';
 import CounterBox from './comp/CounterBox.js';
 
+// -- replace <comp> in DOM, and returns {counter1, counter2...}
 const comps = compReplace([
   Counter("counter1", {count: 5}),
   Counter("counter2"),
   CounterBox("counterbox1")
 ]);
 
-
+// -- resetting from outside
+// -- using parent counterbox
 document.querySelector('.reset-counters').addEventListener('click',()=>{
   [comps.counter1, comps.counter2, comps.counterbox1]
     .forEach(c=>c.update({count:-5}));
 })
 
 // -- optionally using comp's $
+// -- accession counterbox children
 $('.reset-counters-each').addEventListener('click',()=>{
   [comps.counter1, comps.counter2, ...comps.counterbox1.state.counters]
     .forEach((c,i)=>c.update({count:-i}));
