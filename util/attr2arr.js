698032389ec132a5f7d499e13e0f376c3c407dd9
drodsou <gitman@drodsou.com>
2023-04-09 22:12:46 +0200

    storeDOM ok

--- util/attr2arr.js
+++ util/attr2arr.js
@@ -1,4 +1,5 @@
 export default function attr2arr (str) {
+  if (!str) return [];
   return str.split(';').map(a=>a.trim()).filter(a=>a.length)
    .map(a=>a.replace(/(^\||\|$)/g,'').split('|').map(b=>b.trim()))
 }
